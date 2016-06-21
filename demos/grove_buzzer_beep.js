99ac7aec4bd55cc874f3c3602eea5829b1f58428
Zelin <makerdiary@gmail.com>
2016-06-21 15:26:00 +0000

    add grove buzzer beep demo

---
+++ demos/grove_buzzer_beep.js
@@ -0,0 +1,48 @@
+var GrovePi = require('node-grovepi').GrovePi;
+var Commands = GrovePi.commands;
+var Board = GrovePi.board;
+
+var board;
+
+// Connect the Grove Buzzer to digital port D3
+var buzzer = 3;
+
+function start() {
+    console.log('starting...');
+    board = new Board({
+        debug: true,
+        onError: function(err) {
+            console.log('Oops! Something error.');
+            console.log(err);
+        },
+        onInit: function(res) {
+            if(res) {
+                console.log('GrovePi Version : ' + board.version());
+            }
+        }
+    });
+    board.init();
+    board.pinMode(buzzer,"output");
+
+    var buzzerState = true;
+    
+    setInterval(function() {
+        board.writeBytes(Commands.dWrite.concat([buzzer, buzzerState?1:0, Commands.unused]));
+        buzzerState = !buzzerState;
+    }, 1000);
+}
+
+function onExit(err) {
+  console.log('ending');
+  board.close();
+  process.removeAllListeners();
+  process.exit();
+  if (typeof err != 'undefined');
+    console.log(err);
+}
+
+//start the app
+start();
+
+// catches ctrl+c event
+process.on('SIGINT', onExit);
