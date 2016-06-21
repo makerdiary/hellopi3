32fa6886330509c2bd631a5112d06db8d338a522
Zelin Cai <makerdiary@gmail.com>
2016-06-22 00:21:37 +0800

    add grove button

---
+++ demos/grove_button.js
@@ -0,0 +1,48 @@
+var GrovePi = require('node-grovepi').GrovePi;
+var Commands = GrovePi.commands;
+var Board = GrovePi.board;
+var GroveButton = GrovePi.sensors.genericDigitalInput;
+
+var board;
+
+// Connect the Grove Button to digital port D2
+
+function start() {
+    console.log('starting...');
+    board = new Board({
+        debug: true,
+        onError: function(err) {
+            console.log('Oops! Something error.');
+            console.log(err);
+        },
+        onInit: function(res) {
+            if(res) {
+                console.log('GrovePi Version : ' + board.version());
+                
+                var button = new GroveButton(2);
+
+                button.on('change', function(res) {
+                    console.log('Grove Button value= ' + res);
+                });
+                button.watch();
+            }
+        }
+    });
+    board.init();
+}
+
+function onExit(err) {
+    console.log('ending');
+    board.close();
+    process.removeAllListeners();
+    process.exit();
+    if (typeof err != 'undefined')
+        console.log(err);
+}
+
+
+//start the app
+start();
+
+// catches ctrl+c event
+process.on('SIGINT', onExit);
